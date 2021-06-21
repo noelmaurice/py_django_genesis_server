629726e3dbbbcf1525e209f61fe4753152521f0b
maurice_n <maurice.noel@iuct-oncopole.fr>
2021-06-21 17:12:10 +0200

    Save

--- variant_project/urls.py
+++ variant_project/urls.py
@@ -6,7 +6,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     re_path(r'^$', RedirectView.as_view(url='variant/')),
 
-    # re_path(r'^sample/', include('sample.urls', namespace='sample')),
     re_path(r'^variant/', include('variant.urls', namespace='variant')),
 
     re_path(r'^ws/variant/', include('variant.urlsWS', namespace='variantWS')),
