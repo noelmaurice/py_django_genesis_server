6d678fe4de64b2310a807a5e625ef2fa0ad0759f
NOEL <noelmaurice@gmail.com>
2021-06-23 17:41:22 +0200

    Database managed object admin site configured

--- sample/serializers.py
+++ sample/serializers.py
@@ -10,8 +10,6 @@ class PartSerializer(serializers.ModelSerializer):
 
 
 class SampleSerializer(serializers.ModelSerializer):
-
-    # the related parts of the sample
     values = PartSerializer(many=True, source='part_set')
 
     class Meta:
