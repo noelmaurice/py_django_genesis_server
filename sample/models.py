40b01002e07713877cc32e5515e18d207b21783f
NOEL <noelmaurice@gmail.com>
2021-06-23 17:41:22 +0200

    Database managed object admin site configured

--- sample/models.py
+++ sample/models.py
@@ -1,16 +1,17 @@
-
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
 class Sample(models.Model):
     name = models.CharField(max_length=200, help_text='Sample name')
     pub_date = models.DateTimeField('date published', auto_now_add=True)
+    filters = ArrayField(models.CharField(max_length=200, help_text='Filters'))
 
     class Meta:
         db_table = 'sample'
 
     def __str__(self):
-        return self.name
+        return self.name + ' (' + str(self.id) + ')'
 
 
 class Part(models.Model):
