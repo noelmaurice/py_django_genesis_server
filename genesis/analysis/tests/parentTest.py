0a8534ec769db1f7fdc9add22e1cfc2bb51b7cee
maurice_n <maurice.noel@iuct-oncopole.fr>
2021-07-28 14:41:04 +0200

    save

--- genesis/analysis/tests/parentTest.py
+++ genesis/analysis/tests/parentTest.py
@@ -1,8 +1,32 @@
+import json
+
+import requests
 from django.test import TestCase
+from requests import Response
+from rest_framework import status, request
+
 
 class ParentTest(TestCase):
     """
     All the test objects managed by django test inherit of ParentTest
     """
 
-    pass
+    # server parameters
+    HOST: str = 'localhost'
+    PORT: int = 8000
+
+    @staticmethod
+    def postWS(url: str, data_json: str) -> Response:
+        """
+        The post web service
+        :param url: web service url
+        :param data_json: data to recorded
+        :return: response object in accordance with the request
+        """
+        data_dict = json.loads(data_json)
+
+        request = url.format(
+                    host = ParentTest.HOST,
+                    port = ParentTest.PORT)
+
+        return requests.post(request, json=data_dict)
--- genesis/analysis/viewsWS.py
+++ genesis/analysis/viewsWS.py
@@ -3,65 +3,67 @@ Analysis web services
 """
 
 from rest_framework import generics
-from genesis.analysis.model_data.serializers.analysisSerializer import *
+
+from genesis.analysis.model_data.analysisData import *
+
 
 class SampleDetail(generics.CreateAPIView):
     queryset = Sample.objects.all()
-    serializer_class = SampleSerializer
+    serializer_class = Sample.get_serializer()
 
 
 class SampleTagDetail(generics.CreateAPIView):
     queryset = SampleTag.objects.all()
-    serializer_class = SampleTagSerializer
+    serializer_class = SampleTag.get_serializer()
 
 
 class ProviderDetail(generics.CreateAPIView):
     queryset = Provider.objects.all()
-    serializer_class = ProviderSerializer
+    serializer_class = Provider.get_serializer()
 
 
 class InstrumentDetail(generics.CreateAPIView):
     queryset = Instrument.objects.all()
-    serializer_class = InstrumentSerializer
+    serializer_class = Instrument.get_serializer()
 
 
 class RunDetail(generics.CreateAPIView):
     queryset = Run.objects.all()
-    serializer_class = RunSerializer
+    serializer_class = Run.get_serializer()
 
 
 class RunTagDetail(generics.CreateAPIView):
     queryset = RunTag.objects.all()
-    serializer_class = RunTagSerializer
+    serializer_class = RunTag.get_serializer()
 
 
 class ResultDetail(generics.CreateAPIView):
     queryset = Result.objects.all()
-    serializer_class = ResultSerializer
+    serializer_class = Result.get_serializer()
 
 
 class SoftwareDetail(generics.CreateAPIView):
     queryset = Software.objects.all()
-    serializer_class = SoftwareSerializer
+    serializer_class = Software.get_serializer()
 
 
 class AnalysisDetail(generics.CreateAPIView):
     queryset = Analysis.objects.all()
-    serializer_class = AnalysisSerializer
+    serializer_class = Analysis.get_serializer()
 
 
 class ResultConsumerDetail(generics.CreateAPIView):
     queryset = ResultConsumer.objects.all()
-    serializer_class = ResultConsumerSerializer
+    serializer_class = ResultConsumer.get_serializer()
 
 
 class SampleResultDetail(generics.CreateAPIView):
     queryset = SampleResult.objects.all()
-    serializer_class = SampleResultSerializer
+    serializer_class = SampleResult.get_serializer()
 
 
 class RunSampleDetail(generics.CreateAPIView):
     queryset = RunSample.objects.all()
-    serializer_class = RunSampleSerializer
+    serializer_class = RunSample.get_serializer()
 
 
